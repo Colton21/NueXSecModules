2dd644badfb68548e7208356a60dccac4bd545e0
Krishan Mistry <kvjmistry@gmail.com>
2019-03-09 21:42:42 -0600

    commit run variations script

--- scripts/run_variations.sh
+++ scripts/run_variations.sh
@@ -1,4 +1,5 @@
 # bash script to run all the variations in one go
+# To run do: source run_variations.sh
 run(){
     COLOR="\033[1;33m"
     COLOR="\033[1;33m"
@@ -14,6 +15,16 @@ run './main.exe --var_mode files/filter_deadSaturatedChannels.root "";'
 run './main.exe --var_mode files/filter_altDeadChannels.root "";'
 run './main.exe --var_mode files/filter_EnhancedTPCVis.root "";'
 run './main.exe --var_mode files/filter_squeezeResp.root "";'
+run './main.exe --var_mode files/filter_upPEnoise.root "";'
+run './main.exe --var_mode files/filter_noiseAmpDown.root "";'
+run './main.exe --var_mode files/filter_downPEnoise.root "";'
+run './main.exe --var_mode files/filter_noiseAmpUp.root "";'
+run './main.exe --var_mode files/filter_DTdown.root "";'
+run './main.exe --var_mode files/filter_DTup.root "";'
+run './main.exe --var_mode files/filter_DLdown.root "";'
+run './main.exe --var_mode files/filter_DLup.root "";'
+run './main.exe --var_mode files/filter_dataSCE.root "";'
+run './main.exe --var_mode files/filter_LArG4BugFix.root "";'
 
 # run this again for saving the plot
 run './main.exe --var_mode files/filter_EnhancedTPCVis.root "same";'
